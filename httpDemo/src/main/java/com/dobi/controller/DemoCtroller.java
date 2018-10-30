5a17954a9b45ed5bacc6a51edb8a7f7a7566f588
dobi <549914283@qq.com>
2018-10-30 23:45:21 +0800

    http

--- httpDemo/src/main/java/com/dobi/controller/DemoCtroller.java
+++ httpDemo/src/main/java/com/dobi/controller/DemoCtroller.java
@@ -1,6 +1,7 @@
 package com.dobi.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,14 @@ public class DemoCtroller {
         System.out.println("请求成功--"+username+":"+password);
         return "请求成功";
     }
+
+    @RequestMapping("/doPost")
+    public String doPost(String username,String password){
+
+        System.out.println("请求成功--"+username+":"+password);
+        return "请求成功";
+    }
+
+
 
 }
